feffe04585d6b30586ef63a9befed6ddbbb32371
Vasanthan <vasanth2754@gmail.com>
2020-10-15 12:53:57 +0530

    Updates

---
+++ precache-manifest.ed73816c7f93b2e8c863020074a0ab36.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b1e59a92d524dbd6b62f11482851f3f",
+    "url": "/react-product-landing-page/index.html"
+  },
+  {
+    "revision": "354aa6552dd49fd60830",
+    "url": "/react-product-landing-page/static/css/main.bd2c5ebf.chunk.css"
+  },
+  {
+    "revision": "a10ff59a7d071e0a3542",
+    "url": "/react-product-landing-page/static/js/2.c92c45db.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-product-landing-page/static/js/2.c92c45db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "354aa6552dd49fd60830",
+    "url": "/react-product-landing-page/static/js/main.15bd5b30.chunk.js"
+  },
+  {
+    "revision": "60f51e75ea6a87d1259a",
+    "url": "/react-product-landing-page/static/js/runtime-main.fbd2dfb9.js"
+  },
+  {
+    "revision": "c1f9993b4fce219491d533f3f358fd86",
+    "url": "/react-product-landing-page/static/media/svg-1.c1f9993b.svg"
+  },
+  {
+    "revision": "b61c4fbeecd73fc412296f8f182c94bd",
+    "url": "/react-product-landing-page/static/media/svg-2.b61c4fbe.svg"
+  },
+  {
+    "revision": "f7f12d8b700f6d8f96ca0c84a7a366e3",
+    "url": "/react-product-landing-page/static/media/svg-3.f7f12d8b.svg"
+  },
+  {
+    "revision": "df6f0a596e1f4d3308b3002f10e14e16",
+    "url": "/react-product-landing-page/static/media/svg-4.df6f0a59.svg"
+  },
+  {
+    "revision": "6f625599018bee7f81f51ac1c4a8037f",
+    "url": "/react-product-landing-page/static/media/svg-5.6f625599.svg"
+  },
+  {
+    "revision": "ac1714cd810cd6bfafac15a1cdb1e2d1",
+    "url": "/react-product-landing-page/static/media/svg-6.ac1714cd.svg"
+  },
+  {
+    "revision": "f8d962976858d4a33ff661d7bbe70c3a",
+    "url": "/react-product-landing-page/static/media/video.f8d96297.mp4"
+  }
+]);
